e6358d286973e4b327f7b9b3a17cec0e7f6ad837
Philip <philipgabriel21@gmail.com>
2024-12-28 16:50:38 -0700

    Made some minor alterations

--- Scripts/net_simulation.py
+++ Scripts/net_simulation.py
@@ -6,20 +6,33 @@ import random
 server = ModbusServer("127.0.0.1", 502, no_block=True)
 server.start()
 print("Modbus server running...")
+v_values = [10, 20, 50, 120, 140, 180, 240]
+i_values = [1, 2, 3, 30, 35, 40, 45, 50]
+i = 1
+
+file_path = r'C:\\Users\\phili\\source\\repos\\SCADA\\DataLog.csv'
+
+
+# Open the file in write mode, which clears the file content
+with open(file_path, 'w') as file:
+    file.write('')
 
 try:
     while True:
         # Simulate changing values for registers
-        voltage = random.randint(210, 240)  # Simulate voltage
-        current = random.randint(10, 30)   # Simulate current
-        breaker_status = random.choice([True, False])  # Breaker ON/OFF
+        voltage = random.choice(v_values)  # Simulate voltage
+        current = random.choice(i_values)   # Simulate current
+        if voltage < 100 or current < 10:
+            breaker_status = False
+        else:
+            breaker_status = True
 
         # Write values to Modbus DataBank
         server.data_bank.set_holding_registers(0, [voltage])  # Voltage at 40001
         server.data_bank.set_holding_registers(1, [current])  # Current at 40002
         server.data_bank.set_coils(0, [breaker_status])  # Breaker at 00001
-
-        print(f"Voltage: {voltage}, Current: {current}, Breaker: {breaker_status}")
+        print(f"{i} Voltage: {voltage}, Current: {current}, Breaker: {breaker_status}")
+        i += 1
         time.sleep(1)
 
 except KeyboardInterrupt:
